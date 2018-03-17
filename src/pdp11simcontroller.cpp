30e12f317d48f40f0e1b1f6e537402063ea8f29b
schnelled <32557878+schnelled@users.noreply.github.com>
2018-03-16 20:50:02 -0700

    One bug left

--- src/pdp11simcontroller.cpp
+++ src/pdp11simcontroller.cpp
@@ -905,7 +905,8 @@ OctalWord PDP11SimController::REGISTER_DEFERRED(OctalWord regValue, int reg)
 OctalWord PDP11SimController::AUTOINC(OctalWord regValue, int reg)
 {
 	//Print to the trace file (data read)
-	PRINT_TO_FILE(regValue + 2, 0);
+	PRINT_TO_FILE(regValue, 0);
+	//PRINT_TO_FILE(memory.getWord(regValue), 0);
 
 	//Obtain the value from memory (pointer)
 	return memory.getWord(regValue);
@@ -921,7 +922,8 @@ OctalWord PDP11SimController::AUTOINC(OctalWord regValue, int reg)
 OctalWord PDP11SimController::AUTOINC_DEFERRED(OctalWord regValue, int reg)
 {
 	//Print to the trace file (data read)
-	PRINT_TO_FILE(memory.getWord(regValue + 2), 0);
+	PRINT_TO_FILE(regValue, 0);
+	PRINT_TO_FILE(memory.getWord(regValue), 0);
 
 	//Obtain the value from memory (pointer)
 	return memory.getWord(memory.getWord(regValue));
@@ -992,8 +994,14 @@ OctalWord PDP11SimController::INDEX_DEFERRED(OctalWord regValue, int reg)
 {
 	pc.setval(pc.getVal() + 2);
 	OctalWord location = memory.getWord(pc.getVal()) + regValue;
+
+	PRINT_TO_FILE(pc.getVal() + regValue, 0);
+	PRINT_TO_FILE(location, 0);
+
 	OctalWord pointer = memory.getWord(location);
 	OctalWord operand = memory.getWord(pointer);
+
+	PRINT_TO_FILE(pointer, 0);
 
 	return operand;
 }
